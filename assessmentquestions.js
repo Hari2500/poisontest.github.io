7d86d000ac6f758a72150b95b6d2f64b1df91fa2
Hari2500 <133198478+Hari2500@users.noreply.github.com>
2023-12-31 23:27:12 +0530

    Add files via upload

---
+++ assessmentquestions.js
@@ -0,0 +1,156 @@
+const quizData = [
+  {
+    question:
+      "<img src='banjo/headcrack.png' style='height: 200px' '></img>",
+    //"<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/headcrack.png' style='height: 200px' '></img>",
+    value: "q1",
+    options: [
+      "Head Burr",
+      "Head Dent Mark",
+      "Head Pit Mark",
+      "Head Crack",
+    ],
+    answer: "Head Crack",
+  },
+  {
+    question:
+      "<img src='banjo/shankpitmark.png' style='height: 200px' '> </img>",
+    //"<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/shankpitmark.png' style='height: 200px' '> </img>",
+    value: "q2",
+    options: [
+      "Shank Damage",
+      "Shank Pit Mark",
+      "Poor Finish",
+      "Shank Line Mark",
+    ],
+    answer: "Shank Pit Mark",
+  },
+  {
+    question:
+      "<img src='banjo/headdentmark.png' style='height: 200px' '> </img>",
+    //"<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/headdentmark.png' style='height: 200px' '> </img>",
+    value: "q3",
+    options: [
+      "Head Crack",
+      "Head Damage",
+      "Head Pit Mark",
+      "Head Dent Mark",
+    ],
+    answer: "Head Dent Mark",
+  },
+  {
+    question:
+      "<img src='banjo/linemark.png' style='height: 200px' '> </img>",
+    //"<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/linemark.png' style='height: 200px' '> </img>",
+    value: "q4",
+    options: [
+      "Shank Damage",
+      "Shank Dent Mark",
+      "Shank Line Mark",
+      "Head Dent Mark",
+    ],
+    answer: "Shank Line Mark",
+  },
+  {
+    question:
+      "<img src='banjo/pitmark.png' style='height: 200px' '> </img>",
+    //"<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/pitmark.png' style='height: 200px' '> </img>",
+    value: "q5",
+    options: [
+      "Head Chamber Uneven",
+      "Head Burr",
+      "Head Damage",
+      "Head Pit Mark",
+    ],
+    answer: "Head Pit Mark",
+  },
+  {
+    question:
+      "<img src='banjo/shankdamage.png' style='height: 200px' '> </img>",
+    //"<h2 style = 'text-align: center'>Select the defect  <br> दोष का चयन करें </h2> <img src='banjo/shankdamage.png' style='height: 200px' '> </img>",
+    value: "q6",
+    options: [
+      "Poor Finish",
+      "Shank Dent Mark",
+      "Head Crack",
+      "Shank Damage",
+    ],
+    answer: "Shank Damage",
+  },
+  {
+    question:
+      "<img src='banjo/shankdamage1.png' style='height: 200px' '> </img>",
+    //"<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/shankdamage1.png' style='height: 200px' '> </img>",
+    value: "q7",
+    options: [
+      "Poor Finish",
+      "Shank Dent Mark",
+      "Head Crack",
+      "Shank Damage",
+    ],
+    answer: "Shank Damage",
+  },
+  {
+    question:
+      "<img src='banjo/acrossflatunderfill.png' style='height: 200px' '> </img>",
+    //"<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/acrossflatunderfill.png' style='height: 200px' '> </img>",
+    value: "q8",
+    options: [
+      "Across Flat Under Fill",
+      "Head Dent Mark",
+      "Poor Finish",
+      "Improper Finish",
+    ],
+    answer: "Across Flat Under Fill",
+  },
+  {
+    question:
+      "<img src='banjo/acrossflatunderfill1.png' style='height: 200px' '> </img>",
+    //"<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/acrossflatunderfill1.png' style='height: 200px' '> </img>",
+    value: "q9",
+    options: [
+      "Improper Finish",
+      "Head Crack",
+      "Poor Finish",
+      "Across Flat Under Fill",
+    ],
+    answer: "Across Flat Under Fill",
+  },
+  {
+    question:
+      "<img src='banjo/headdentmark2.png' style='height: 200px' '> </img>",
+    //`<h2 style = 'text-align: center'>Select the defect <br> दोष का चयन करें </h2> <img src='banjo/headdentmark2.png' style='height: 200px' '> </img>`,
+    value: "q10",
+    options: [
+      "Head Burr",
+      "Head Pit Mark",
+      "Head Damage",
+      "Head Dent Mark",
+    ],
+    answer: "Head Dent Mark",
+  },
+  /*
+{
+  question: "<img src='groupedImages/pointingdamage.jpg' style='height: 200px' '> </img>",
+  options: ['Bearing Face Convex', 'Bearing Face Underfill', 'Pointing Damage', 'White Patches'],
+  answer: 'Pointing Damage',
+},
+{
+  question: "<img src='groupedImages/poorfinish.jpg' style='height: 200px' '> </img>",
+  options: ['Poor Finish', 'Poor MEC Coating', 'Thread Damage', 'Shank Damage'],
+  answer: 'Poor Finish',
+},
+{
+  question: "<img src='groupedImages/poorfinish1.jpg' style='height: 200px' '> </img>",
+  options: ['Shank Damage', 'Poor Finish', 'Thread Damage', 'Poor MEC Coating'],
+  answer: 'Poor Finish',
+},
+{
+  question: "<img src='groupedImages/poormeccoating.jpg' style='height: 200px' '> </img>",
+  options: ['Poor Finish', 'Dent Mark', 'Improper MEC Coating', 'Poor MEC Coating'],
+  answer: 'Poor MEC Coating',
+},
+
+
+*/
+];
